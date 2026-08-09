018ac5791af63f063583b1cfd57ec7a9f0892a51
Samirchauhan1 <chauhansamir560@gmail.com>
2026-08-09 21:47:22 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'node:path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+base: '/SPEAKEASY/',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
